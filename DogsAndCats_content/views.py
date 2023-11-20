28b4dfd3e4182bf28be9eeb67dfc845561513433
Kamil <orlowskik97@gmail.com>
2023-11-20 17:53:11 +0100

    Adjusting IndexView

--- DogsAndCats_content/views.py
+++ DogsAndCats_content/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic.base import TemplateView
 from .models import Pet
 
 
 # Create your views here.
-class IndexView(ListView):
+class IndexView(TemplateView):
     template_name = 'DogsAndCats_content/index.html'
-    context_object_name = 'dogs_and_cats'
 
-    def get_queryset(self):
-        return Pet.objects.all()
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['pets'] = Pet.objects.all()
+        context['kinds'] = Pet.objects.values('kind').distinct()
+        return context
+
+
 
 
